60fe8a7dc3eab4c3b372e741df3fbe15abe92a1a
imdarlings <dhinestroza880@soyudemedellin.edu.co>
2025-10-23 09:48:48 -0500

    TRAMPAS

---
+++ MIS_FRESAS/Assets/_Main/Code/Scripts/Trampa.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Trampa : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameManager.instance.RestarVida();
+        }
+    }
+}
